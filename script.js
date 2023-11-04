56632af62c09a001ff0ad78e7f26b960cf953094
SkyeH-M <justskye98@gmail.com>
2023-11-04 11:34:20 +0000

    Working on ending score

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+let options = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
-  let options = ["Rock", "Paper", "Scissors"];
   // get random index value
   let randomIndex = Math.floor(Math.random() * options.length);
   // get random array item
@@ -7,33 +8,50 @@ function getComputerChoice() {
   return option;
 }
 
-// function playerChoice() {
-//   let playerSelection = window.prompt("Please pick Rock, Paper, or Scissors");
-//   return playerChoice;
-// }
-
-// playerChoice();
-
-function gamePlay(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return "Tie!";
+function playerChoice() {
+  let validatedInput = false;
+  while (validatedInput == false) {
+    let playerSelection = window.prompt("Please pick Rock, Paper, or Scissors");
+    // if ()
   }
-  if (
+  return playerSelection;
+}
+
+playerChoice();
+
+function checkWinner(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return "Tie";
+  } else if (
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "scissors" && computerSelection == "paper") ||
     (playerSelection == "paper" && computerSelection == "rock")
   ) {
-    return "You win!";
-  }
-  if (
-    (computerSelection == "rock" && playerSelection == "scissors") ||
-    (computerSelection == "scissors" && playerSelection == "paper") ||
-    (computerSelection == "paper" && playerSelection == "rock")
-  ) {
-    return "Computer wins!";
+    return "Player";
+  } else {
+    return "Computer";
   }
 }
 
-let playerSelection = "Rock";
-let computerSelection = getComputerChoice();
-console.log(gamePlay(playerSelection, computerSelection));
+function gamePlay(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  let result = checkWinner(playerSelection, computerSelection);
+  if (result == "Tie") {
+    return "It's a Tie!";
+  } else if (result == "Player") {
+    return `Congratulations! You win, ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `Computer wins, ${computerSelection} beats ${playerSelection}. Try again!`;
+  }
+}
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = playerChoice();
+    let computerSelection = getComputerChoice();
+    console.log(playerSelection, computerSelection);
+    console.log(gamePlay(playerSelection, computerSelection));
+  }
+}
+
+let begin = game();
